4acf5c0b57c0a4b556378efb78fab9b0b8443a52
XiaoMin27 <calista.ng.ca@gmail.com>
2026-02-02 15:05:22 +0800

    testtest

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("wowowoowow");
+﻿//==========================================================
+// Student Number : S10268653
+// Student Name : Ng Sook Min Calista
+// Partner Name : Dayana
+//==========================================================
+
+// 1.
+foreach (var r in restaurants)
+{
+    Console.WriteLine($"Restaurant: {r.Name}");
+
+    foreach (var item in r.FoodItems)
+    {
+        Console.WriteLine(
+            $" - {item.ItemName}: {item.Description} (${item.Price:0.00})"
+        );
+    }
+
+    Console.WriteLine();
+}
+
+//
