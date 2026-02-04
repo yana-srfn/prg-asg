0c29d588f8a5168a8c35f44363b9c88f50bd4ddd
yana-srfn <srfn.dayana@gmail.com>
2026-02-04 22:48:58 +0800

    edited

--- Program.cs
+++ Program.cs
@@ -1,16 +1,17 @@
-//Lists for all classes
+namespace PRGAssignment
+{
+    class Program
+    {
+        // Lists for all classes
+        static List<Customer> customers = new List<Customer>();
+        static List<Restaurant> restaurants = new List<Restaurant>();
+        static List<FoodItem> foodItems = new List<FoodItem>();
+        static List<Order> orders = new List<Order>();
+        static List<SpecialOffer> specialOffers = new List<SpecialOffer>();
 
-//Customers
-static List<Customer> customers = new List<Customer>();
-
-//Restaurants
-static List<Restaurant> restaurants = new List<Restaurant>();
-
-//Food items
-static List<FoodItem> foodItems = new List<FoodItem>();
-
-//Orders
-static List<Order> orders = new List<Order>();
-
-//Special Offers
-static List<SpecialOffer> specialOffers = new List<SpecialOffer>();
+        static void Main(string[] args)
+        {
+            // You can call feature methods here
+        }
+    }
+}
