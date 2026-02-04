622d736d0bb7d27c83c7358161f49dfb23d80cd9
yana <srfn.dayana@gmail.com>
2026-02-04 22:46:21 +0800

    class declarations and static lists

--- Program.cs
+++ Program.cs
@@ -1,64 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.IO;
-using System.Linq;
+//Lists for all classes
 
-namespace PRGAssignment
-{
-    class Program
-    {
-        //Classes
+//Customers
+static List<Customer> customers = new List<Customer>();
 
-        public class Customer
-        {
-            public string Name { get; set; }
-            public string Email { get; set; }
-            public List<Order> OrderList { get; set; } = new List<Order>();
-        }
+//Restaurants
+static List<Restaurant> restaurants = new List<Restaurant>();
 
-        public class FoodItem
-        {
-            public string RestaurantId { get; set; }
-            public string ItemName { get; set; }
-            public string Description { get; set; }
-            public decimal Price { get; set; }
-        }
+//Food items
+static List<FoodItem> foodItems = new List<FoodItem>();
 
-        public class Restaurant
-        {
-            public string RestaurantId { get; set; }
-            public string Name { get; set; }
-            public string Email { get; set; }
-            public List<FoodItem> MenuItems { get; set; } = new List<FoodItem>();
-            public Queue<Order> OrderQueue { get; set; } = new Queue<Order>();
-        }
+//Orders
+static List<Order> orders = new List<Order>();
 
-        public class Order
-        {
-            public string OrderId { get; set; }
-            public Customer Customer { get; set; }
-            public Restaurant Restaurant { get; set; }
-            public DateTime DeliveryDateTime { get; set; }
-            public string DeliveryAddress { get; set; }
-            public List<(FoodItem Item, int Quantity)> Items { get; set; } = new List<(FoodItem, int)>();
-            public decimal TotalAmount { get; set; }
-            public string Status { get; set; }
-        }
-
-        public class SpecialOffer
-        {
-            public string RestaurantId { get; set; }
-            public string OfferCode { get; set; }
-            public string Description { get; set; }
-            public decimal DiscountAmount { get; set; }
-        }
-
-        // ==================== Lists ====================
-        static List<Customer> customers = new List<Customer>();
-        static List<Restaurant> restaurants = new List<Restaurant>();
-        static List<FoodItem> foodItems = new List<FoodItem>();
-        static List<Order> orders = new List<Order>();
-        static List<SpecialOffer> specialOffers = new List<SpecialOffer>();
-    }
-}
+//Special Offers
+static List<SpecialOffer> specialOffers = new List<SpecialOffer>();
