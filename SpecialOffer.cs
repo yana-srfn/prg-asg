bcd4f780b59332f562762959bd5224d98ad5cd62
yana <srfn.dayana@gmail.com>
2026-02-04 23:09:16 +0800

    Update SpecialOffer.cs

--- SpecialOffer.cs
+++ SpecialOffer.cs
@@ -2,7 +2,7 @@ namespace PRGAssignment
 {
     public class SpecialOffer
     {
-        public string RestaurantId { get; set; }   // maps to the restaurant
+        public string RestaurantId { get; set; }  
         public string OfferCode { get; set; }
         public string Description { get; set; }
         public decimal DiscountAmount { get; set; }
