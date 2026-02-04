a85f764137019d2ef923dc17a42f26c8f74a8fdc
yana-srfn <srfn.dayana@gmail.com>
2026-02-04 23:15:01 +0800

    editted

--- Program.cs
+++ Program.cs
@@ -1,17 +1,21 @@
+using System;
+using System.Collections.Generic;
+
 namespace PRGAssignment
 {
     class Program
     {
-        // Lists for all classes
-        static List<Customer> customers = new List<Customer>();
-        static List<Restaurant> restaurants = new List<Restaurant>();
-        static List<FoodItem> foodItems = new List<FoodItem>();
-        static List<Order> orders = new List<Order>();
-        static List<SpecialOffer> specialOffers = new List<SpecialOffer>();
+        // ==================== Shared Lists ====================
+        public static List<Customer> customers = new List<Customer>();
+        public static List<Restaurant> restaurants = new List<Restaurant>();
+        public static List<FoodItem> foodItems = new List<FoodItem>();
+        public static List<Order> orders = new List<Order>();
+        public static List<SpecialOffer> specialOffers = new List<SpecialOffer>();
 
         static void Main(string[] args)
         {
-            // You can call feature methods here
+            // You can optionally call features here for testing
+            Console.WriteLine("Master branch loaded with shared lists.");
         }
     }
 }
