84c58368109f6beed05cb14597bbb4ff8d60fc5a
yana-srfn <srfn.dayana@gmail.com>
2026-02-04 22:14:12 +0800

    editted

--- Program.cs
+++ Program.cs
@@ -189,3 +189,8 @@ namespace PRGAssignment
             Console.Write("Enter Restaurant ID: ");
             string restId = Console.ReadLine();
             var rest = restaurants.FirstOrDefault(r => r.RestaurantId == restId);
+            static void CreateOrder()
+            {
+                Console.WriteLine("Feature 5 - CreateOrder() TODO: implement fully.");
+            }
+        }
