4fae9f1e8c170797f672576e9a387e8fc00f73a2
XiaoMin27 <calista.ng.ca@gmail.com>
2026-02-04 22:50:38 +0800

    test

---
+++ Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prg_asg
+{
+    class Customer
+    {
+        public string Name { get; }
+        public string Email { get; }
+
+        public List<Order> Orders { get; }
+
+        public Customer(string name, string email)
+        {
+            Name = name;
+            Email = email;
+            Orders = new List<Order>();
+        }
+    }
+}
