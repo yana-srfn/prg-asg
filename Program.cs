ab41704a653c74ebf60f22fe093644be519d919e
yana-srfn <srfn.dayana@gmail.com>
2026-02-04 21:08:18 +0800

    empty dayana-branch

--- Program.cs
+++ Program.cs
@@ -1,5 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("fnaijfae");
-Console.WriteLine("test");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("wowowoowow");
+﻿
