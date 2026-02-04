4a014c67b6b18731c1990e49cec475e9f4af1646
yana <srfn.dayana@gmail.com>
2026-02-04 22:43:29 +0800

    Create FoodItem class with properties

---
+++ FoodItem.cs
@@ -0,0 +1,10 @@
+namespace PRGAssignment
+{
+    public class FoodItem
+    {
+        public string RestaurantId { get; set; }
+        public string ItemName { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
