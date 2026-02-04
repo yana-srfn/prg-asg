d36d847943b56aa79c5f88c1840c677cd3991b06
yana <srfn.dayana@gmail.com>
2026-02-04 22:44:59 +0800

    Add SpecialOffer class for restaurant offers

---
+++ SpecialOffer.cs
@@ -0,0 +1,10 @@
+namespace PRGAssignment
+{
+    public class SpecialOffer
+    {
+        public string RestaurantId { get; set; }   // maps to the restaurant
+        public string OfferCode { get; set; }
+        public string Description { get; set; }
+        public decimal DiscountAmount { get; set; }
+    }
+}
