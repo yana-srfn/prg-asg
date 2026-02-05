d425e2a7286d8bc582dd8172b2237e6def2990ca
XiaoMin27 <calista.ng.ca@gmail.com>
2026-02-05 10:16:43 +0800

    did classes

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@
 // Student Name : Ng Sook Min Calista
 // Partner Name : Dayana
 //==========================================================
-using prg_asg;
+using PRGAssignment;
 using System;
 using System.Collections.Generic;
 using System.Globalization;
@@ -12,26 +12,61 @@ using System.Linq;
 
 
 // 1.
-List<Restaurant> restaurants = new List<Restaurant>(); // haven't pull the class needed in github yet
+List<Restaurant> restaurants = new List<Restaurant>();
+//Load Files
+void LoadRestaurants(string path)
+{
+    foreach (var line in File.ReadAllLines(path).Skip(1))
+    {
+        var p = line.Split(',');
+        restaurants.Add(new Restaurant(p[0], p[1], p[2]));
+    }
+}
+void LoadFoodItems(string path)
+{
+    foreach (var line in File.ReadAllLines(path).Skip(1))
+    {
+        var p = line.Split(',');
+        string restId = p[0].Trim();
+        string itemName = p[1].Trim();
+        string itemDesc = p[2].Trim();
+        double itemPrice = double.Parse(p[3].Trim(), CultureInfo.InvariantCulture);
+
+        Restaurant r = restaurants.FirstOrDefault(x => x.GetRestaurantId() == restId);
+        if (r == null) continue;
+
+        // Ensure the restaurant has at least 1 menu (Main Menu)
+        if (r.GetMenus().Count == 0)
+        {
+            r.AddMenu(new Menu("M1", "Main Menu"));
+        }
+
+        // Create food item (customise not in CSV -> empty string)
+        FoodItem fi = new FoodItem(itemName, itemDesc, itemPrice, "");
+
+        // Add food item into the first menu
+        r.GetMenus()[0].AddFoodItem(fi);
+    }
+}
+LoadRestaurants("restaurants.csv");
+LoadFoodItems("fooditems.csv");
+
+
 // Feature 1: List restaurants & food items
 foreach (var r in restaurants)
 {
-    Console.WriteLine($"Restaurant: {r.Name}");
+        Console.WriteLine($"Restaurant: {r.GetRestaurantName()}");
 
-    foreach (var item in r.FoodItems)
-    {
-        Console.WriteLine(
-            $" - {item.ItemName}: {item.Description} (${item.Price:0.00})"
-        );
-    }
+        // Restaurant contains menus, menus contain food items
+        r.DisplayMenu();
 
-    Console.WriteLine();
+        Console.WriteLine();
 }
 
 //4
 Console.WriteLine("OrderID  Customer        Restaurant       Delivery Time        Amount   Status");
 
-foreach (var o in Orders) // haven't pull the class needed in github yet
+/*foreach (var o in Orders) // haven't pull the class needed in github yet
 {
     Console.WriteLine(
         $"{o.OrderId,-7} " +
@@ -41,7 +76,7 @@ foreach (var o in Orders) // haven't pull the class needed in github yet
         $"${o.TotalAmount,-7:0.00} " +
         $"{o.Status}"
     );
-}
+}*/
 
 //6
 //8
--- Restaurant.cs
+++ Restaurant.cs
@@ -1,13 +1,75 @@
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 
 namespace PRGAssignment
 {
     public class Restaurant
     {
-        public string RestaurantId { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public List<FoodItem> MenuItems { get; set; } = new List<FoodItem>();
-        public Queue<Order> OrderQueue { get; set; } = new Queue<Order>();
+        private string restaurantId;
+        private string restaurantName;
+        private string restaurantEmail;
+
+        // Relationship
+        private List<Menu> menu = new List<MenuCommand>();
+        private Queue<Order> orderQueue;
+
+        public string RestaurantId
+        {
+            get { return restaurantId; }
+            set { restaurantId = value; }            
+        }
+            
+
+        public string RestaurantName
+        {
+            get { return restaurantName; }
+            set { restaurantName = value; }
+        }
+        public string RestaurantEmail
+        {
+            get { return restaurantEmail; }
+            set { restaurantEmail = value; }
+        }
+        public List<Menu> GetMenus() { return menus; }
+
+        // +DisplayMenu() : void
+        public void DisplayMenu()
+        {
+            // Show each menu and its food items
+            foreach (Menu m in menus)
+            {
+                Console.WriteLine($"Menu: {m.ToString()}");
+                m.DisplayFoodItems();
+            }
+        }
+        // +AddMenu(Menu) : void
+        public void AddMenu(Menu menu)
+        {
+            menus.Add(menu);
+        }
+
+        // +RemoveMenu(Menu) : bool
+        public bool RemoveMenu(Menu menu)
+        {
+            return menus.Remove(menu);
+        }
+
+        // (These are in diagram but you can implement later)
+        public void DisplayOrders()
+        {
+            Console.WriteLine("DisplayOrders() not implemented yet.");
+        }
+        public void DisplaySpecialOffers()
+        {
+            Console.WriteLine("DisplaySpecialOffers() not implemented yet.");
+        }
+
+        public override string ToString()
+        {
+            return { restaurantId }
+            ;
+        }
     }
+
+}
 }
