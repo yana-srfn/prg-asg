6655185182ee17ed345b702700c6aab089dfd4bd
XiaoMin27 <calista.ng.ca@gmail.com>
2026-02-05 16:41:59 +0800

    added a todo list

--- Program.cs
+++ Program.cs
@@ -1,8 +1,41 @@
-//==========================================================
+﻿//==========================================================
 // Student Number : S10268653
 // Student Name : Ng Sook Min Calista
 // Partner Name : Dayana
 //==========================================================
+
+/* 
+ TODO:
+ 1.Load files (restaurants and food items)
+- load the restaurant.csv file
+- create the Restaurant objects based on the data loaded
+- load the fooditems.csv file
+- create the FoodItem objects and assign them to their respective restaurants 
+
+4. List all orders with basic information *Output in writeup pdf
+- display ALL orders with the required details: Order ID, Customer Name, Restaurant Name, Delivery Time, Total Amount, and Order Status 
+
+6. Process an order *Output in writeup pdf
+- prompt the user to enter a restaurant ID
+- Loop thru’ the Order Queue and display each order for processing.
+- display the basic information of the order (order could be pending, cancelled or preparing)
+- prompt the user to enter [C] for confirming, [R] for rejecting the order, [S] for skipping or [D] for delivering the order .
+    • If confirming, check that the order status is "Pending" and update the order status to "Preparing".
+    • If rejecting, check that the order status is "Pending", update the order status to "Rejected", add the order to the Refund Stack and display refund message.
+    • If skipping, check that the order status is "Cancelled" and move on to next order.
+    • If delivering, check that the order status is "Preparing" and update the order status to "Delivered".
+- display a message indicating the outcome. 
+
+8.Delete an existing order *Output in writeup pdf
+- prompt the user to enter a Customer Email
+- display all orders from the Order List that are "Pending"
+- prompt the user to enter an Order ID
+- display the basic information of the order
+- prompt user to confirm deletion
+- update the status of the order to “Cancelled”, add the order to the Refund Stack and display refund message
+- display a message indicating the outcome. 
+*/
+
 using PRGAssignment;
 using System;
 using System.Collections.Generic;
@@ -75,17 +108,7 @@ foreach (var r in restaurants)
 //4
 Console.WriteLine("OrderID  Customer        Restaurant       Delivery Time        Amount   Status");
 
-/*foreach (var o in Orders) // haven't pull the class needed in github yet
-{
-    Console.WriteLine(
-        $"{o.OrderId,-7} " +
-        $"{o.Customer.Name,-15} " +
-        $"{o.Restaurant.Name,-15} " +
-        $"{o.DeliveryDateTime:dd/MM/yyyy HH:mm,-20} " +
-        $"${o.TotalAmount,-7:0.00} " +
-        $"{o.Status}"
-    );
-}*/
+
 
 //6
 //8
