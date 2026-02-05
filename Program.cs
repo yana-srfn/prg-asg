317f40518d8bb557a07cc4c0812610fb1ffb7ac9
yana-srfn <srfn.dayana@gmail.com>
2026-02-05 10:58:40 +0800

    deleted class program

--- Program.cs
+++ Program.cs
@@ -10,36 +10,34 @@ using System.Collections.Generic;
 
 namespace PRGAssignment
 {
-    class Program
+    //Shared Lists
+    public static List<Customer> customers = new List<Customer>();
+    public static List<Restaurant> restaurants = new List<Restaurant>();
+    public static List<FoodItem> foodItems = new List<FoodItem>();
+    public static List<Order> orders = new List<Order>();
+    public static List<SpecialOffer> specialOffers = new List<SpecialOffer>();
+
+    static void Main(string[] args)
     {
-        //Shared Lists
-        public static List<Customer> customers = new List<Customer>();
-        public static List<Restaurant> restaurants = new List<Restaurant>();
-        public static List<FoodItem> foodItems = new List<FoodItem>();
-        public static List<Order> orders = new List<Order>();
-        public static List<SpecialOffer> specialOffers = new List<SpecialOffer>();
+        //Load all CSVs into master lists
+        LoadCustomers();
+        LoadRestaurants();
+        LoadFoodItems();
+        LoadOrders();
+        LoadSpecialOffers();
 
-        static void Main(string[] args)
+        //Interactive menu for features
+        while (true)
         {
-            // Load all CSVs into master lists
-            LoadCustomers();
-            LoadRestaurants();
-            LoadFoodItems();
-            LoadOrders();
-            LoadSpecialOffers();
-
-            // Interactive menu for features
-            while (true)
-            {
-                Console.WriteLine("\nWelcome to the Gruberoo Food Delivery System");
-                Console.WriteLine("  restaurants loaded!");
-                Console.WriteLine("  food items loaded!");
-                Console.WriteLine("  customers loaded!");
-                Console.WriteLine("  orders loaded!");
-                Console.Write("Choice: ");
-                string choice = Console.ReadLine();
-            }
+            Console.WriteLine("\nWelcome to the Gruberoo Food Delivery System");
+            Console.WriteLine("  restaurants loaded!");
+            Console.WriteLine("  food items loaded!");
+            Console.WriteLine("  customers loaded!");
+            Console.WriteLine("  orders loaded!");
+            Console.Write("Choice: ");
+            string choice = Console.ReadLine();
         }
     }
+    
 }
 
