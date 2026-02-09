660a281cb8bb6b748513c9178b103d68a3ee9114
yana-srfn <srfn.dayana@gmail.com>
2026-02-09 10:00:28 +0800

    editted

---
+++ OrderedFoodItem.cs
@@ -0,0 +1,35 @@
+﻿using PRGAssignment;
+using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class OrderedFoodItem
+{
+    // ===== attributes =====
+    private FoodItem foodItem;
+    private int qtyOrdered;
+    private double subTotal;
+
+    // ===== constructor =====
+    public OrderedFoodItem(FoodItem foodItem, int qtyOrdered)
+    {
+        this.foodItem = foodItem;
+        this.qtyOrdered = qtyOrdered;
+    }
+
+    // ===== methods =====
+    public double CalculateSubtotal()
+    {
+        subTotal = foodItem.Price * qtyOrdered;
+        return subTotal;
+    }
+
+    public override string ToString()
+    {
+        return $"{foodItem.ItemName} - {qtyOrdered}";
+    }
+}
+
