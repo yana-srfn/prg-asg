a7580d70de54b789220925fc59f585a17a49bcba
yana-srfn <srfn.dayana@gmail.com>
2026-02-09 09:22:49 +0800

    editted class

--- Customer.cs
+++ Customer.cs
@@ -1,35 +1,33 @@
 using System.Collections.Generic;
+using PRGAssignment;
 
-namespace PRGAssignment
+public class Customer
 {
-    public class Customer
+    private string emailAddress;
+    private string customerName;
+
+
+    public string CustomerName { get { return customerName; } set { customerName = value; } }
+    public string EmailAddress { get { return emailAddress; } set { emailAddress = value; } }
+    public List<Order> OrderList { get { return Order; } set { Order = value; } } = new List<Order>();
+
+    public void AddOrder(Order order)
     {
-        private string emailAddress;
-        private string customerName;
+        OrderList.Add(order);
+    }
 
-
-        public string CustomerName { get { return customerName; } set { customerName = value; } }
-        public string EmailAddress { get { return emailAddress; } set { emailAddress = value; } }
-        public List<Order> OrderList { get { return Order; } set { Order = value; } } = new List<Order>();
-
-        public void AddOrder(Order order)
+    public void DisplayAllOrders()
+    {
+        foreach (Order order in OrderList)
         {
-            OrderList.Add(order);
+            System.Console.WriteLine(order);
         }
+    }
 
-        public void DisplayAllOrders()
-        {
-            foreach (Order order in OrderList)
-            {
-                System.Console.WriteLine(order);
-            }
-        }
+    public bool RemoveOrder(Order order) { return OrderList.Remove(order); }
 
-        public bool RemoveOrder(Order order) {return OrderList.Remove(order); }
-
-        public override string ToString()
-        {
-            return $"Customer Name: {CustomerName}, Email Address: {EmailAddress}";
-        }
+    public override string ToString()
+    {
+        return $"Customer Name: {CustomerName}, Email Address: {EmailAddress}";
     }
 }
--- Order.cs
+++ Order.cs
@@ -1,31 +1,29 @@
+using PRGAssignment;
 using System;
 using System.Collections.Generic;
 
-namespace PRGAssignment
+public class Order
 {
-    public class Order
+    private int orderId;
+    private DateTime orderDateTime;
+    private double orderTotal;
+    private string orderStatus;
+    private DateTime deliveryDateTime;
+    private string deliveryAddress;
+    private string orderPaymentMethod;
+    private bool orderPaid;
+
+
+    public int OrderId { get; set; }
+    public double CalculateOrderTotal() { return orderTotal; }
+    public void AddOrderedFoodItem(FoodItem item, int quantity) { }
+
+    public bool RemoveOrderedFoodItem(FoodItem item) { return true; }
+    public void DisplayOrderedFoodItems() { }
+
+    public override string ToString()
     {
-        private int orderId;
-        private DateTime orderDateTime;
-        private double orderTotal;
-        private string orderStatus;
-        private DateTime deliveryDateTime;
-        private string deliveryAddress;
-        private string orderPaymentMethod;
-        private bool orderPaid;
-
-
-        public int OrderId { get; set; }
-        public double CalculateOrderTotal() { return orderTotal; }
-        public void AddOrderedFoodItem(FoodItem item, int quantity) { }
-
-        public bool RemoveOrderedFoodItem(FoodItem item) { return true; }
-        public void DisplayOrderedFoodItems() { }
-
-        public override string ToString()
-        {
-            return $"Order ID: {OrderId}, Total Amount: {orderTotal}, Status: {orderStatus}";
-        }
-
+        return $"Order ID: {OrderId}, Total Amount: {orderTotal}, Status: {orderStatus}";
     }
+
 }
