70395a1dcd0750a148b01c0ff8667b347704ebf1
XiaoMin27 <calista.ng.ca@gmail.com>
2026-02-09 10:31:15 +0800

    added menu

--- Menu.cs
+++ Menu.cs
@@ -1,10 +1,73 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//==========================================================
+// Student Number : S10275337C
+// Student Name : Dayana Sharafeena
+// Student Number : S10268653
+// Student Name : Ng Sook Min Calista
+//==========================================================
+
+using System;
 
 namespace PRGAssignment
 {
-   
+    public class Menu
+    {
+        private string menuId;
+        private string menuName;
+        private List<FoodItem> foodItems;
+
+        public Menu(string menuId, string menuName)
+        {
+            this.menuId = menuId;
+            this.menuName = menuName;
+            foodItems = new List<FoodItem>();
+        }
+
+        public string MenuId
+        {
+            get { return menuId; }
+            set { menuId = value; }
+        }
+
+        public string MenuName
+        {
+            get { return menuName; }
+            set { menuName = value; }
+        }
+
+        public void AddFoodItem(FoodItem foodItem)
+        {
+            foodItems.Add(foodItem);
+        }
+
+        public bool RemoveFoodItem(FoodItem foodItem)
+        {
+            return foodItems.Remove(foodItem);
+        }
+
+        public void DisplayFoodItems()
+        {
+            Console.WriteLine($"== Menu: {menuName} ({menuId}) ==");
+            if (foodItems.Count == 0)
+            {
+                Console.WriteLine("No food items in this menu.");
+                return;
+            }
+
+            foreach (FoodItem fi in foodItems)
+            {
+                Console.WriteLine(fi);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Menu ID: {menuId}\nMenu Name: {menuName}\nFood Items: {foodItems.Count}";
+        }
+
+        public List<FoodItem> GetFoodItems()
+        {
+            return foodItems;
+        }
+    }
 }
+
