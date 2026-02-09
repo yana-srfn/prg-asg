bb7bfcd618e0fd022417cd643ec03ca78058360a
yana <srfn.dayana@gmail.com>
2026-02-09 10:24:29 +0800

    Refactor SpecialOffer class attributes and constructor
    Refactored SpecialOffer class to use private fields and a constructor for initialization.

--- SpecialOffer.cs
+++ SpecialOffer.cs
@@ -5,13 +5,51 @@
 // Student Name : Ng Sook Min Calista
 //==========================================================
 
+using System;
+
 namespace PRGAssignment
 {
     public class SpecialOffer
     {
-        public string RestaurantId { get; set; }  
-        public string OfferCode { get; set; }
-        public string Description { get; set; }
-        public decimal DiscountAmount { get; set; }
+        // ===== attributes =====
+        private string restaurantId;
+        private string offerCode;
+        private string description;
+        private double discountAmount;
+
+        // ===== constructor =====
+        public SpecialOffer(string restaurantId, string offerCode, string description, double discountAmount)
+        {
+            this.restaurantId = restaurantId;
+            this.offerCode = offerCode;
+            this.description = description;
+            this.discountAmount = discountAmount;
+        }
+
+        // ===== properties =====
+        public string RestaurantId
+        {
+            get { return restaurantId; }
+        }
+
+        public string OfferCode
+        {
+            get { return offerCode; }
+        }
+
+        public string Description
+        {
+            get { return description; }
+        }
+
+        public double DiscountAmount
+        {
+            get { return discountAmount; }
+        }
+
+        public override string ToString()
+        {
+            return $"{offerCode} - {description} (${discountAmount:0.00} off)";
+        }
     }
 }
