7810898b3d28782d5febbfc8abce1194562f0599
yana-srfn <srfn.dayana@gmail.com>
2026-02-09 09:27:18 +0800

    editted

--- Customer.cs
+++ Customer.cs
@@ -6,13 +6,36 @@
 //==========================================================
 
 using System.Collections.Generic;
+using PRGAssignment;
+using System;
 
-namespace PRGAssignment
+public class Customer
 {
-    public class Customer
+    private string emailAddress;
+    private string customerName;
+
+
+    public string CustomerName { get { return customerName; } set { customerName = value; } }
+    public string EmailAddress { get { return emailAddress; } set { emailAddress = value; } }
+    public List<Order> OrderList { get { return Order; } set { Order = value; } } = new List<Order>();
+
+    public void AddOrder(Order order)
     {
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public List<Order> OrderList { get; set; } = new List<Order>();
+        OrderList.Add(order);
+    }
+
+    public void DisplayAllOrders()
+    {
+        foreach (Order order in OrderList)
+        {
+            System.Console.WriteLine(order);
+        }
+    }
+
+    public bool RemoveOrder(Order order) { return OrderList.Remove(order); }
+
+    public override string ToString()
+    {
+        return $"Customer Name: {CustomerName}, Email Address: {EmailAddress}";
     }
 }
